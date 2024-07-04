1d199f89afe9b3c19489696f422938606f8b4e05
RohithKumar4 <uit20211@rmd.ac.in>
2024-07-04 20:55:24 +0530

    initial

--- main.js
+++ main.js
@@ -23,3 +23,22 @@ jQuery(document).ready(function ($) {
         time: 1000
     });
 });
+
+// ------map-----
+
+function initMap() {
+    // The location of your business or desired location
+    var location = { lat: 40.7128, lng: -74.0060 }; // Example: New York City coordinates
+
+    // The map, centered at your location
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: location,
+    });
+
+    // The marker, positioned at your location
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
